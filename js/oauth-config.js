a11d978573126ffbf458df34bc8a2db72b96304a
Adm. Jesus <134287070+jesusmjunior@users.noreply.github.com>
2025-04-26 17:24:02 -0300

    Create oauth-config.js

---
+++ js/oauth-config.js
@@ -0,0 +1,5 @@
+  // oauth-config.js
+  const OAUTH_CONFIG = {
+      CLIENT_ID: '145009781337-r0q8eejsk724ushkoo9u129ioe6hdunn.apps.googleusercontent.com',
+      API_KEY: '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', // Preencha com sua API Key
+      SCOPES: 'https://www.googleapis.com/auth/drive.file
